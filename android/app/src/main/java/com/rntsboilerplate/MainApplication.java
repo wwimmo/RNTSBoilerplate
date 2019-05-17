db18a1383512e6adbf4b9b87827e414c60e54b90
WWIMMO\TST <thomas.steinbruechel@wwimmo.ch>
2019-05-17 10:13:46 +0200

    Link react-native-device-info

--- android/app/src/main/java/com/rntsboilerplate/MainApplication.java
+++ android/app/src/main/java/com/rntsboilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.rntsboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNLocalizePackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
