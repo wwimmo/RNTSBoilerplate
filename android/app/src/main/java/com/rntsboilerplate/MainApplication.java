ea6c87972ffe68a4c1958cd89e357695fbe221d2
WWIMMO\TST <thomas.steinbruechel@wwimmo.ch>
2019-05-17 11:35:13 +0200

    Link netinfo and async storage after adding from community

--- android/app/src/main/java/com/rntsboilerplate/MainApplication.java
+++ android/app/src/main/java/com/rntsboilerplate/MainApplication.java
@@ -3,6 +3,8 @@ package com.rntsboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.rnfs.RNFSPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
@@ -28,6 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
+            new NetInfoPackage(),
             new RNFSPackage(),
             new RNDeviceInfo(),
             new RNLocalizePackage(),
