908f570b546a152a24f3781f67fb2531b7975887
WWIMMO\TST <thomas.steinbruechel@wwimmo.ch>
2019-05-17 10:57:25 +0200

    Link react-native-fs

--- android/app/src/main/java/com/rntsboilerplate/MainApplication.java
+++ android/app/src/main/java/com/rntsboilerplate/MainApplication.java
@@ -3,6 +3,7 @@ package com.rntsboilerplate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new RNDeviceInfo(),
             new RNLocalizePackage(),
             new VectorIconsPackage(),
